0f13c35c791b63888268af71f4db757c400cf101
Kyle <kylevance801@gmail.com>
2022-09-23 13:14:01 -0600

    requires setup, db connection created, schema formated

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mysql = require('mysql2');
+const consoleTable = require('console.table');
+const inquirer = require('inquirer');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "@Irf0rce",
+    database: "tracker_db"
+},
+console.log("Employee Tracker Database loaded")
+);
