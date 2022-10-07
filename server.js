0cf20b7799d103beafa093803b60e0e08a38ac9b
Kyle <kylevance801@gmail.com>
2022-10-06 18:36:36 -0600

    added the add functions and the update role function. Appears to be working as intended.

--- server.js
+++ server.js
@@ -94,3 +94,168 @@ function viewEmployees() {
             menu();
         });
 }
+
+function addDepartment() {
+    inquirer
+        .prompt([
+            {
+                name: "department",
+                type: "input",
+                message: "Enter the name of the department."
+            }
+        ]).then((data) => {
+            db.query(
+                'INSERT INTO department VALUES (DEFAULT, ?)', [data.department],
+                console.log(`You have added ${data.department}.`),
+                menu()
+            );
+        });
+}
+
+function addRole() {
+    db.query("SELECT * FROM department", (err, data) => {
+        if (err) throw err;
+        inquirer
+            .prompt([
+                {
+                    name: "title",
+                    type: "input",
+                    message: "Enter the name of the new role."
+                },
+                {
+                    name: "salary",
+                    type: "number",
+                    message: "Enter the salary of this role.",
+                },
+                {
+                    name: "deptartment_id",
+                    type: "list",
+                    message: "Enter the department ID for this role.",
+                    choices: () => {
+                        const departmentsArray = [];
+                        for (const departments of data) {
+                            departmentsArray.push(departments.id);
+                        }
+                        return departmentsArray;
+                    }
+                }
+
+            ]).then((data) => {
+                db.query(
+                    'INSERT INTO role SET ?',
+                    {
+                        title: data.title,
+                        salary: data.salary,
+                        department_id: data.deptartment_id
+                    },
+                    function (err) {
+                        if (err) throw err;
+                        console.log(`You have added ${data.title}.`),
+                            menu()
+                    }
+                );
+            });
+    });
+}
+
+function addEmployee() {
+    db.query("SELECT * FROM role", (err, data) => {
+        if (err) throw err;
+        inquirer
+            .prompt([
+                {
+                    name: "first_name",
+                    type: "input",
+                    message: "Enter the employee's first name."
+                },
+                {
+                    name: "last_name",
+                    type: "input",
+                    message: "Enter the employee's last name.",
+                },
+                {
+                    name: "role_id",
+                    type: "list",
+                    message: "Enter employee's role ID.",
+                    choices: () => {
+                        const roleArray = [];
+                        for (const roleId of data) {
+                            roleArray.push(roleId.id)
+                        }
+                        return roleArray;
+                    }
+                },
+                {
+                    name: "manager",
+                    type: "list",
+                    message: "Enter the employee's manager ID.",
+                    choices: [1, 2, 3, 4]
+                }
+
+            ]).then((data) => {
+                db.query(
+                    'INSERT INTO employee SET ?',
+                    {
+                        first_name: data.first_name,
+                        last_name: data.last_name,
+                        role_id: data.role_id,
+                        manager_id: data.manager
+                    },
+                    console.log(`You have added ${data.first_name} ${data.last_name}.`),
+                    menu()
+                );
+            });
+    });
+}
+
+// Update Function
+function updateEmployee() {
+    db.query("SELECT * FROM employee", (err, data) => {
+        if (err) throw err;
+        inquirer
+            .prompt([
+                {
+                    name: "name",
+                    type: "list",
+                    message: "Select an employee's to update their role.",
+                    choices: () => {
+
+                        const nameArray = [];
+                        for (const last_name of data) {
+                            nameArray.push(last_name.last_name);
+                        }
+                        return nameArray;
+                    }
+                },
+                {
+                    name: "title",
+                    type: "list",
+                    message: "Enter the role ID of this employee.",
+                    choices: () => {
+                        const roleArray = [];
+                        for (const role of data) {
+                            roleArray.push(role.role_id);
+                        }
+                        return roleArray;
+                    }
+                }
+            ]).then((data) => {
+                let last_name = data.name;
+                db.query(
+                    "UPDATE employee SET ? WHERE last_name = ?",
+                    [
+                        {
+                            role_id: data.title
+                        }, last_name
+                    ],
+                    console.log(`You have updated ${last_name}'s role.`),
+                    menu()
+                );
+            });
+    });
+}
+
+
+// app.listen(PORT, () => {
+//     console.log(`Server live on port ${PORT}.`);
+// });
